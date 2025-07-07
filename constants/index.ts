8bdcbc5801786033751dc914033f3bb5c4a339d6
Kamran0925 <kamranrizwan0925@gmail.com>
2025-07-07 15:08:59 +0500

    Add constants for travelexa

---
+++ constants/index.ts
@@ -0,0 +1,75 @@
+// NAVIGATION
+export const NAV_LINKS = [
+  { href: "/", key: "home", label: "Home" },
+  { href: "/", key: "how_travelexa_work", label: "How Travelexa Work?" },
+  { href: "/", key: "services", label: "Services" },
+  { href: "/", key: "pricing ", label: "Pricing " },
+  { href: "/", key: "contact_us", label: "Contact Us" },
+];
+
+// CAMP SECTION
+export const PEOPLE_URL = ["/person-1.png", "/person-2.png", "/person-3.png"];
+
+// FEATURES SECTION
+export const FEATURES = [
+  {
+    title: "Offline Navigation Support",
+    icon: "/map.svg",
+    variant: "green",
+    description:
+      "Access detailed maps even without internet. Perfect for remote hikes and mountain trails with no network coverage.",
+  },
+  {
+    title: "Plan Trips with Ease",
+    icon: "/calendar.svg",
+    variant: "green",
+    description:
+      "Create and manage your travel itinerary effortlessly. Sync plans with friends and enjoy stress-free adventures.",
+  },
+  {
+    title: "AR-Powered Trail Guidance",
+    icon: "/tech.svg",
+    variant: "green",
+    description:
+      "Navigate using augmented reality for an immersive hiking experience — works seamlessly even offline.",
+  },
+  {
+    title: "Fresh Destinations Monthly",
+    icon: "/location.svg",
+    variant: "orange",
+    description:
+      "Discover new places added every month by our global explorer community. Adventure never gets old.",
+  },
+];
+
+// FOOTER SECTION
+export const FOOTER_LINKS = [
+  {
+    title: "Learn More",
+    links: [
+      "Press Releases",
+      "Environment",
+      "Privacy Policy",
+      "Contact Us",
+      "Jobs",
+      "About",
+    ],
+  },
+  {
+    title: "Explorer Community",
+    links: ["Summit Stories", "Hiking with Travelexa", "Kinthill Adventures"],
+  },
+];
+
+export const FOOTER_CONTACT_INFO = {
+  title: "Contact Us",
+  links: [
+    { label: "Email Us", value: "travelexa@akinthil.com" },
+    { label: "Administration", value: "123-456-7890" },
+  ],
+};
+
+export const SOCIALS = {
+  title: "Social Links",
+  links: ["/facebook.svg", "/instagram.svg", "/twitter.svg"],
+};
